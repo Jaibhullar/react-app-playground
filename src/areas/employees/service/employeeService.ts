ad7a4e7d606b26e849da073a0261ee7f99bd3a28
Jaibhullar <jai.bhullar@brighthr.com>
2026-03-26 16:43:19 +0000

    feat: implement employee service layer with DTOs and API integration

---
+++ src/areas/employees/service/employeeService.ts
@@ -0,0 +1,71 @@
+import { API_BASE_URL } from '@/common/constants';
+
+import type { Employee } from '../types';
+
+export type DTO_Employee = {
+	id: number,
+	name: string,
+	department: {
+		id: number,
+		name: string,
+	},
+	location: {
+		id: number,
+		name: string,
+	},
+	role: {
+		id: number,
+		name: string,
+	},
+};
+
+export type DTO_GetEmployeesResponse = {
+	employees: DTO_Employee[],
+};
+
+export type GetEmployeesRequest = {
+	departmentId?: number,
+	locationId?: number,
+	roleId?: number,
+};
+
+export type GetEmployeesResponse = {
+	employees: Employee[],
+};
+
+const getEmployeesRoute = 'employees:department=:departmentId&location=:locationId&role=:roleId' as const;
+
+function transformDTO(dto: DTO_Employee) {
+	const { id, name, department, location, role } = dto;
+	const employee : Employee = {
+		id,
+		name,
+		department,
+		location,
+		role,
+	};
+	return employee;
+}
+
+export async function executeGetEmployees(request: GetEmployeesRequest) {
+	const url = getQueryUrl(request);
+
+	// Note there is no error handling and we are using base fetch here for demo simplicity.
+	return fetch(url)
+		.then(response=>
+			response.json()
+		)
+		.then(json=>{
+			const responseDTO = json as DTO_GetEmployeesResponse;
+			const response : GetEmployeesResponse = { employees: responseDTO.employees.map(transformDTO) };
+			return response;
+		});
+}
+
+
+function getQueryUrl(request: GetEmployeesRequest) {
+	const departmentId = request.departmentId ?? 'all';
+	const locationId = request.locationId ?? 'all';
+	const roleId = request.roleId ?? 'all';
+	return `${API_BASE_URL}${getEmployeesRoute.replace(':departmentId', String(departmentId)).replace(':locationId', String(locationId)).replace(':roleId', String(roleId))}`;
+}
